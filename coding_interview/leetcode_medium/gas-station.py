1a8c8382bb0e6f0ebe177afae0b8d5746f942b9c
Alexey Abramov <abitualex@gmail.com>
2023-07-18 13:10:55 +0300

    updated Gas Station solution

--- coding_interview/leetcode_medium/gas-station.py
+++ coding_interview/leetcode_medium/gas-station.py
@@ -1,23 +1,15 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         diff = [g-c for g,c in zip(gas, cost)]
-        if sum(diff)<0:
-            return -1
         starting_idx = 0
-        visited = [False for _ in diff]
         cur_gas = 0
-        idx = -1
-        gas_len = len(gas)
-        while True:
-            idx = (idx+1)%gas_len
-            new_idx = (idx+1)%gas_len
-            visited[idx] = True
-            cur_gas += diff[idx]
+        sum = 0
+        for i in range(len(diff)):
+            cur_gas += diff[i]
+            sum += diff[i]
             if cur_gas < 0:
-                if visited[new_idx]==True:
-                    return -1
-                else:
-                    starting_idx = new_idx
-                    cur_gas = 0
-            elif new_idx == starting_idx:
-                return starting_idx
+                cur_gas = 0
+                starting_idx = i + 1
+        if sum < 0:
+            return -1
+        return starting_idx
